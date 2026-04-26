a72e9260ee9b517a29aa249a8d79ba250fcd3bf6
DaniEgeaCortacero <130358354+DaniEgeaCortacero@users.noreply.github.com>
2026-04-26 19:37:51 +0200

    Cambios

--- init/02-import-all.sh
+++ init/02-import-all.sh
@@ -8,11 +8,11 @@ find /imports -type f -name "*.sql" ! -path "/imports/dataset-meteo/*" | sort | 
     psql -U "$POSTGRES_USER" -d "torquelab" -f "$file"
 done
 
-echo "=== Importando scripts de dataset-meteo en PRACTICASM ==="
+echo "=== Importando scripts de dataset-meteo en PracticasSM ==="
 
 find /imports/dataset-meteo -type f -name "*.sql" | sort | while read -r file; do
-    echo "Ejecutando en PRACTICASM: $file"
-    psql -U "$POSTGRES_USER" -d "PRACTICASM" -f "$file"
+    echo "Ejecutando en PracticasSM : $file"
+    psql -U "$POSTGRES_USER" -d "PracticasSM" -f "$file"
 done
 
 echo "=== Importación finalizada ==="
